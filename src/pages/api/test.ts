bf3d71f927418f91e6f91ecb43f4245cdec7be9d
Vinicius <solutiontecforyou@gmail.com>
2021-11-14 23:22:03 -0300

    test: remove debug process.env

--- src/pages/api/test.ts
+++
@@ -1,6 +0,0 @@
-import { BaseResponseType } from '@/types/request';
-import type { NextApiRequest, NextApiResponse } from 'next';
-
-export default function handler(req: NextApiRequest, res: NextApiResponse<BaseResponseType>) {
-  return res.status(200).json({ message: '', success: true, body: process.env });
-}
