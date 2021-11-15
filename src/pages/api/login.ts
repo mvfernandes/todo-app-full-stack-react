ce9621bbe0d9f934489c34ea634cca65569ac02f
Vinicius <solutiontecforyou@gmail.com>
2021-11-15 11:02:43 -0300

    refact: add message not found key

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -6,6 +6,7 @@ export default function handler(req: NextApiRequest, res: NextApiResponse<BaseRe
     if (JSON.parse(req.body)?.key === process.env.SECRET_KEY) {
       return res.status(200).json({ message: 'u are loged', success: true });
     }
+    return res.status(404).json({ message: 'key not found', success: false });
   }
   return res.status(404).json({ message: '', success: false });
 }
