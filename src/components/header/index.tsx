18972f71392eec3920061331e063746549cd9586
Vinicius <solutiontecforyou@gmail.com>
2021-11-15 13:21:08 -0300

    fix: remove target blank from a element

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -9,7 +9,16 @@ export function Header() {
     <Container>
       <h2>Todo App</h2>
       <p>App para gerenciamento de tarefas do dia a dia</p>
-      <a href="https://github.com/mvfernandes/todo-app-react-native/blob/main/app-release.apk?raw=true" target="_blank">Baixar app android</a>
+      <a
+        onClick={() => {
+          window.open(
+            'https://github.com/mvfernandes/todo-app-react-native/blob/main/app-release.apk?raw=true',
+            '_blank'
+          );
+        }}
+      >
+        Baixar app android
+      </a>
 
       <button onClick={handleLogout}>Logout</button>
     </Container>
