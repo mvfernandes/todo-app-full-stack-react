f768ed8930716da0f5d8bf3737dd90263f3f89a8
Vinicius <solutiontecforyou@gmail.com>
2021-11-15 02:22:53 -0300

    fix: verify method login

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -2,8 +2,10 @@ import { BaseResponseType } from '@/types/request';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<BaseResponseType>) {
-  if (JSON.parse(req.body)?.key === process.env.SECRET_KEY) {
-    return res.status(200).json({ message: 'u are loged', success: true });
+  if (req.method === 'POST') {
+    if (JSON.parse(req.body)?.key === process.env.SECRET_KEY) {
+      return res.status(200).json({ message: 'u are loged', success: true });
+    }
   }
-  return res.status(404).json({ message: 'this key does not exist', success: false });
+  return res.status(404).json({ message: '', success: false });
 }
